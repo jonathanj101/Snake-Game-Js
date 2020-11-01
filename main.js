afdc82f120258739277e23f0f2089b8efd4ec04a
jonathanj101 <jonatjimen@gmail.com>
2020-11-01 18:55:30 -0500

    refactored code
    refactored code

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-var canvas = document.getElementById('canvas')
-var canvasContext = canvas.getContext("2d")
-var playerScore = document.getElementById("playerScore")
-var highScore = document.getElementById("highScore")
+const canvas = document.getElementById('canvas')
+const canvasContext = canvas.getContext("2d")
+const playerScore = document.getElementById("playerScore")
+const highScore = document.getElementById("highScore")
 
-var coords = 15
-let dir;
+const COORDINATES = 15
 let score = 0
 
 
 window.onload = function () {
-    getFromLocalStorage()
     var snake = {
         body: [
             { x: 150, y: 300 },
-        ]
+        ],
+        direction: undefined
     }
     var apple = {
         x: multiple(15, canvas.width - 15),
@@ -22,32 +21,68 @@ window.onload = function () {
 
     var fps = 15
 
-    setInterval(function () {
-        main(snake, apple)
-        if (snake.body[0].x === apple.x && snake.body[0].y === apple.y) {
-            snake.body.push({ x: snake.body[0].x, y: snake.body[0].y })
-            apple = generateApple(apple)
-            let parsedPlayerScore = Number(playerScore.textContent)
-            playerScore.textContent = parsedPlayerScore + 1
-            score += 1
-            addToLocalStorage(score)
-        }
+    setInterval(main, 1000 / fps, snkae, apple)
 
-    }, 1000 / fps)
+    document.addEventListener('keydown', function (e) {
+        if (e.keyCode === 37 && snake.direction != "RIGHT") {
+            snake.direction = 'LEFT'
+        }
+        if (e.keyCode === 38 && snake.direction != "DOWN") {
+            snake.direction = 'UP'
+        }
+        if (e.keyCode === 39 && snake.direction != "LEFT") {
+            snake.direction = 'RIGHT'
+        }
+        if (e.keyCode === 40 && snake.direction != "UP") {
+            snake.direction = 'DOWN'
+        }
+    })
 }
+
+function isSnakeEatingApple(snake, apple) {
+    return snake.body[0].x === apple.x && snake.body[0].y === apple.y
+
+}
+
+
 function multiple(min, max) {
     return Math.floor(Math.floor(Math.random() * (max + min)) / min) * min
 }
-function main(snake, apple, score) {
+
+function main(snake, apple) {
     createCanvas()
     drawApple(apple)
     drawSnake(snake)
 
 
-    let snakeCopy = createSnakeCopy(snake.body)
-    moveSnake(snake, snakeCopy)
-    gameOver(snake, snakeCopy)
+    moveSnake(snake)
+
+    const isCollidingWithWall = isSnakeCollidingWithWall(sanke) === true
+    const isCollidingWithItSelf = snake.body.length > 1 && isSnakeCollidingWithItSelf(snake) === true
+    if (isCollidingWithWall || isCollidingWithItSelf === true) {
+        location.reload()
+        score = 0
+        alert(`Ooopss! ${isCollidingWithWall ? "Snake hit Wall" : "Snake bite itself"} Game Over!`)
+    }
+
+    if (isSnakeEatingApple(snake, apple) === true) {
+        snake.body.push({ x: snake.body[0].x, y: snake.body[0].y })
+        newApple = generateApple()
+        apple.x = newApple.x
+        apple.y = newApple.y
+        updateScore()
+    }
 }
+
+function updateScore() {
+    updateScoreFromLocalStorage()
+
+    let parsedPlayerScore = Number(playerScore.textContent)
+    playerScore.textContent = parsedPlayerScore + 1
+    score += 1
+    saveScoreToLocalStorage(score)
+}
+
 function createSnakeCopy(snakeBody) {
     let snakeCopy = []
     snakeBody.forEach(snakePart => {
@@ -56,34 +91,16 @@ function createSnakeCopy(snakeBody) {
     return snakeCopy
 }
 function createCanvas() {
-    canvas = document.getElementById('canvas')
-    canvasContext = canvas.getContext("2d")
     createRect(0, 0, canvas.width, canvas.height, 'black')
 }
 function drawApple(apple) {
-    canvas = document.getElementById('canvas')
-    canvasContext = canvas.getContext("2d")
     createRect(apple.x, apple.y, 15, 15, 'green')
 }
 function createRect(x, y, w, h, color) {
     canvasContext.fillStyle = color
     canvasContext.fillRect(x, y, w, h)
 }
-function direction(direction) {
-    if (direction.keyCode === 37 && dir != "RIGHT") {
-        dir = 'LEFT'
-    }
-    if (direction.keyCode === 38 && dir != "DOWN") {
-        dir = 'UP'
-    }
-    if (direction.keyCode === 39 && dir != "LEFT") {
-        dir = 'RIGHT'
-    }
-    if (direction.keyCode === 40 && dir != "UP") {
-        dir = 'DOWN'
-    }
 
-}
 function drawSnake(snake) {
     for (let i = 0; i < snake.body.length; i++) {
         if (i == 0) {
@@ -97,54 +114,54 @@ function drawSnake(snake) {
     }
 
 }
-function moveSnake(snake, snakeCopy) {
+function moveSnake(snake) {
+    let snakeCopy = createSnakeCopy(snake.body)
     for (let i = 0; i < snake.body.length - 1; i++) {
         snake.body[i + 1] = { x: snakeCopy[i].x, y: snakeCopy[i].y }
     }
 
-    if (dir === 'UP') {
-        snake.body[0] = { x: snakeCopy[0].x, y: snakeCopy[0].y - coords }
+    if (snake.direction === 'UP') {
+        snake.body[0] = { x: snakeCopy[0].x, y: snakeCopy[0].y - COORDINAATESs }
     }
-    if (dir === 'DOWN') {
-        snake.body[0] = { x: snakeCopy[0].x, y: snakeCopy[0].y + coords }
+    if (snake.direction === 'DOWN') {
+        snake.body[0] = { x: snakeCopy[0].x, y: snakeCopy[0].y + COORDINAATESs }
     }
-    if (dir === 'RIGHT') {
-        snake.body[0] = { x: snakeCopy[0].x + coords, y: snakeCopy[0].y }
+    if (snake.direction === 'RIGHT') {
+        snake.body[0] = { x: snakeCopy[0].x + COORDINAATESs, y: snakeCopy[0].y }
     }
-    if (dir === 'LEFT') {
-        snake.body[0] = { x: snakeCopy[0].x - coords, y: snakeCopy[0].y }
+    if (snake.direction === 'LEFT') {
+        snake.body[0] = { x: snakeCopy[0].x - COORDINAATESs, y: snakeCopy[0].y }
     }
 }
 function generateApple(apple) {
-    apple = {
+    return {
         x: multiple(15, canvas.width - 15),
         y: multiple(15, canvas.height - 15)
     }
-    return apple
 }
-function gameOver(snake, snakeCopy) {
-    if (snake.body[0].x < -5 || snake.body[0].x > 600 || snake.body[0].y < 0 || snake.body[0].y > 600) {
-        location.reload()
-        score = 0
-        alert("Oooopppss! Snake hit wall! Game Over")
-    }
-    if (snake.body.length > 1) {
-        for (let i = 0; i < snakeCopy.length; i++) {
-            if (snake.body[0].x == snakeCopy[i].x && snake.body[0].y == snakeCopy[i].y) {
-                location.reload()
-                alert("Oooopppss! Snake bite itself! Game Over")
-                score = 0
-            }
+function isSnakeCollidingWithWall(snake) {
+    return (snake.body[0].x < -5 || snake.body[0].x > 600 || snake.body[0].y < 0 || snake.body[0].y > 600)
+}
+function isSnakeCollidingWithItSelf(snake) {
+    for (let i = 1; i < snake.body.length; i++) {
+        if (snake.body[0].x == snake.body[i].x && snake.body[0].y == snake.body[i].y) {
+            return true
         }
     }
+    return false
 }
-function addToLocalStorage(score) {
+function gameOver(snake) {
+
+}
+
+function saveScoreToLocalStorage(score) {
     localStorage.setItem("Score", JSON.stringify(score))
 }
-function getFromLocalStorage() {
+
+function updateScoreFromLocalStorage() {
     score = JSON.parse(localStorage.getItem("Score"))
     if (score > highScore.textContent) {
         highScore.textContent = score
     }
 }
-document.addEventListener('keydown', direction)
+
